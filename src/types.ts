864ddf84a33e66845bbec201c920e097bd0af551
KID-joker <kid_joker@126.com>
2024-08-05 21:15:17 +0800

    WIP: init project

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,21 @@
+import type { FilterPattern } from '@rollup/pluginutils'
+
 export interface Options {
-  // define your plugin options here
+  /**
+   * Only keep uncommitted consoles in the changed files.
+   * If false, keep all consoles.
+   */
+  strict?: boolean
+
+  /**
+   * Remove console type of these module.
+   * @default ['log']
+   */
+  type?: Array<'assert' | 'debug' | 'dir' | 'error' | 'info' | 'log' | 'table' | 'warn'>
+
+  /**
+   * Rules to exclude transforming target.
+   * @default [/node_modules/, /\.git/]
+   */
+  exclude?: FilterPattern
 }
